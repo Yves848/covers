54449fc49cc5afba88a1f2776a1d08b185ead6e3
yves848 <yves.godart@gmail.com>
2019-03-07 12:54:09 +0100

    test

--- test.js
+++ test.js
@@ -1,30 +1,50 @@
 const path = require('path');
 const { promisify } = require('util');
-const Mp3File = require('./src/Classes/mp3filetest');
+//const Mp3File = require('./src/Classes/mp3filetest');
 
-var fs = require("fs");
+var fs = require('fs');
 
-const pathmp3 = path.resolve('./mp3');
+const pathmp3 = path.resolve('d:\\MP3\\');
 
 console.clear();
 console.log(pathmp3);
 
-async function getFiles() {
-    return new Promise((resolve) => {
-        fs.readdir(pathmp3, (err,files) => {
-            resolve(files);
-        })
-    })
-    
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
 
+async function getFiles(aPath) {
+  return new Promise(resolve => {
+    const aFiles=[];
+    console.log('getFiles ',aPath)
+    fs.readdir(aPath,{ withFileTypes: true }, async (err, files) => {
+      console.log(files);
+      await asyncForEach(files,async (dirent,index)=> {
+        if (dirent.isDirectory()) {
+          await getFiles(dirent.name);
+        }
+        else {
+          aFiles.push(dirent.name)
+        }
+      })
+      resolve(aFiles);
+    });
+  });
+}
 
+const main = async () => {
+  const files = await getFiles(pathmp3);
+  //console.log(files);
 
-console.error('DEJA FINI')
+  console.error('DEJA FINI');
 
+  process.stdout.write('\n');
+};
 
+main();
 
-process.stdout.write('\n');
 //console.log(getTags(files[2]));
 //let tag = getTags(files[2]);
 //console.log(tag);
